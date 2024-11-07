4bcddabf6e356d7f77b9044aa9ae73c1ec509b25
Lauri Kivimäki <lauri.kivimaki@opinsys-PTtjXesq>
2024-11-07 12:33:15 +0200

    first commit

---
+++ tehtävä1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main (){
+    int x=1;
+    int y=-2;
+    int z=5;
+
+
+    float keskiarvo = (x+y+z)/3;
+
+    bool b;
+
+    if (x>y){
+        b=true;
+    }
+    else{
+        b=false;
+    }
+
+}
