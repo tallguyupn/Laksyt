f600b477a016a8151b4396e79e3951f14c0c7ed2
Lauri Kivimäki <laurikivimaki012@proton.me>
2024-11-13 12:11:20 +0200

    Tehtävä 6 alku

---
+++ tehtävä6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+void min2();
+void min3();
+void min4();
+
+
+int main (){
+    float x;
+    float y;
+    int valinta;
+
+    std::cout << "Hei. Montako lukua haluaisit antaa? (1-4): ";
+    std::cin >> valinta;
+    switch (valinta){
+        case 1:
+
+    }
+}
