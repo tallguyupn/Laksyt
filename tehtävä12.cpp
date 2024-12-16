08e3e70341c05fff518fe5a8a1ebf05f97f952be
tallguyupn <laurikivimaki012@proton.me>
2024-12-16 16:56:54 +0200

    Tehtävä 12 tehty

--- tehtävä12.cpp
+++ tehtävä12.cpp
@@ -7,6 +7,8 @@ int main () {
     std::string jees;
     char x;
     std::cout << "Hei\nAnna merkkijono: ";
+    
+    //Tulee käyttää getline komentoa sillä std::cin tulostaisi ainoastaan ensimmäisen sanan
     std::getline (std::cin, jees);
     std::cout << "\nMitä merkkiä haluaisit etsiä: ";
     std::cin >> x;
@@ -17,6 +19,7 @@ int main () {
 
 int määrä (const std::string& jees, char ch){
     int count = 0;
+    //Etsitään se tietty kirjain
     for (char c : jees) {
         if (c == ch) {
             count++;
